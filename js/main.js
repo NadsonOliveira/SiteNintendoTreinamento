b3e4a574dbfc0a99b861442d6edf760cda9f11bc
Nadson oliveira <nsoliveira_13@hotmail.com>
2021-03-25 12:49:20 -0300

    first project

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*slider*/
+
+
+$('.slide-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoPlaySpeed: 2000
+});
+
